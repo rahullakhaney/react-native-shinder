ea1ac43604ec4215034eb5b2701ec42f71aa19d8
rahullakhaney <rahul@codebrew.io>
2017-02-12 15:54:35 +0400

    Added app container

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react'
+
+export default class AppContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      store: {},
+      theme: null
+    }
+  }
+  render() {
+    return null
+  }
+}
