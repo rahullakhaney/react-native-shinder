5967d9cb38fe79b52e860ab786a28cda5daf38e5
rahullakhaney <rahul@codebrew.io>
2017-02-12 16:02:51 +0400

    Added side menu logic to appcontainer

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import { Drawer, View } from 'native-base'
 
 export default class AppContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      toggled: false,
       store: {},
       theme: null
     }
   }
+  toggleDrawer() {
+    this.state.toggled ? this._drawer.close() : this._drawer.open()
+  }
+  openDrawer() {
+    this.setState({toggled: true})
+  }
+  closeDrawer() {
+    this.setState({toggled: false})
+  }
   render() {
-    return null
+    return (
+      <Drawer
+      ref={(ref) => this._drawer = ref}
+      type="displace"
+      content={<View style={{backgroundColor: "#000", height: 1000}}
+    />}
+    onClose={this.closeDrawer.bind(this)}
+    onOpen={this.openDrawer.bind(this)}
+    openDrawerOffset={0.2}
+    ></Drawer>
+    )
   }
 }
